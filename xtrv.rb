ab911d141e745da93798dd0e91f13f37874157cf
Tshilidzi Tshivhula <tshilidzi@vontae.co.za>
2020-03-28 16:34:59 +0200

    update xtrv

--- xtrv.rb
+++ xtrv.rb
@@ -1,15 +1,17 @@
 class Xtrv < Formula
   desc "A simple archiving utility"
   homepage ""
-  url "https://github.com/ttshivhula/xtrv/archive/v0.0.1.tar.gz"
-  sha256 "dd47be2bc826a44087a0c8894fd6c96ec8d5cdaed101a37f69552b27743f8c24"
+  url "https://github.com/ttshivhula/xtrv/archive/v0.0.2.tar.gz"
+  sha256 "85ce25abb0a601f702b1a6056a0c20534b73b0ea83d2ae9d9bd2f8963e1489e8"
 
   # depends_on "cmake" => :build
 
   def install
     system "make"
     system "mkdir #{prefix}/bin"
+    system "mkdir #{prefix}/share"
     system "cp xtrv #{prefix}/bin/"
     system "cp unxtrv #{prefix}/bin/"
+    system "cp -rf man #{prefix}/share"
   end
 end
